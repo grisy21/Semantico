ee841633929633bfb091681caece3d2ab1965c4d
grisy21 <grisi8400@gmail.com>
2024-03-10 21:37:22 -0400

    add

---
+++ src/main/java/com/mycompany/semanticos/Semanticos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semanticos;
+
+/**
+ *
+ * @author Grisely Morales
+ */
+public class Semanticos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
